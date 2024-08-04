705936c48b76163af4c2b187f6296cfab5f529ab
Nikola Shirtsa <nikola.shirca@gmail.com>
2024-08-04 08:49:30 -0400

    commit exchange rate entity, repository, service and controller

---
+++ src/main/java/bg/moviebox/repository/ExchangeRateRepository.java
@@ -0,0 +1,13 @@
+package bg.moviebox.repository;
+
+import bg.moviebox.model.entities.ExchangeRateEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ExchangeRateRepository extends JpaRepository<ExchangeRateEntity, Long> {
+  Optional<ExchangeRateEntity> findByCurrency(String currency);
+}
+
---
+++ src/main/java/bg/moviebox/web/CurrencyController.java
@@ -0,0 +1,51 @@
+package bg.moviebox.web;
+
+import bg.moviebox.model.dtos.ConversionResultDTO;
+import bg.moviebox.service.ExchangeRateService;
+import bg.moviebox.service.exception.ApiObjectNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyController {
+
+  private final ExchangeRateService exRateService;
+
+  public CurrencyController(ExchangeRateService exRateService) {
+    this.exRateService = exRateService;
+  }
+
+//  @WarnIfExecutionExceeds(
+//      threshold = 800
+//  )
+  @GetMapping("/api/convert")
+  public ResponseEntity<ConversionResultDTO> convert(
+      @RequestParam("from") String from,
+      @RequestParam("to") String to,
+      @RequestParam("amount") BigDecimal amount
+  ) {
+    BigDecimal result = exRateService.convert(from, to, amount);
+    return ResponseEntity.ok(new ConversionResultDTO(
+        from,
+        to,
+        amount,
+        result
+    ));
+  }
+
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  @ExceptionHandler(ApiObjectNotFoundException.class)
+  @ResponseBody
+  public NotFoundErrorInfo handleApiObjectNotFoundException(ApiObjectNotFoundException apiObjectNotFoundException) {
+    return new NotFoundErrorInfo("NOT FOUND", apiObjectNotFoundException.getId());
+  }
+
+
+  public record NotFoundErrorInfo(
+          String code,
+          Object id) {
+  }
+}
